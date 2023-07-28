141c4d955542d4e527944f5c19b040ff08368680
zainh332 <zain_h332@hotmail.com>
2023-07-28 05:44:39 +0500

    Validation update

--- src/pages/TokenGenerator.vue
+++ src/pages/TokenGenerator.vue
@@ -4,14 +4,14 @@
       <div class="space-y-2 py-8">
         <h1 class="text-t34 text-center font-semibold">Token Generator</h1>
         <p class="text-t16 text-center font-normal">
-          Create and manage your own tokens on the Stellar blockchain with ease using TokenGlade's Token Generator.
+          Create and manage your own tokens on the Stellar blockchain with ease using TokenGlade's Token Generator
         </p>
       </div>
 
       <div class="w-full">
         <div class="flex min-h-full flex-1 flex-col justify-center py-8">
           <div class="w-full">
-            <form class="space-y-6">
+            <Form class="space-y-6" @submit="submitForm" :validationSchema="schema" v-slot=" { errors}">
               <div>
                 <div class="flex items-center justify-between">
                   <label for="ticker" class="block text-t16 font-normal leading-6 text-gray-900">
@@ -20,16 +20,13 @@
                   </label>
                 </div>
                 <div class="mt-2">
-                  <input
-                   v-model="formData.ticker" 
+                  <Field
                    id="ticker" 
                    name="ticker" 
                    type="text" 
                    class="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
-                  <span v-for="error in v$.ticker.$errors" :key="error.$uid" class="text-red-500 text-sm font-normal">
-                    {{ error.$message }}
-                  </span>
+                  <ErrorMessage class="text-red-500 text-sm font-normal" name="ticker" />
                 </div>
               </div>
 
@@ -40,31 +37,57 @@
                   </label>
                 </div>
                 <div class="mt-2">
-                  <input 
-                  v-model="formData.total_supply"
+                  <Field 
                   id="total_supply" 
                   name="total_supply" 
                   type="text"
                   class="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
-                  <span v-for="error in v$.total_supply.$errors" :key="error.$uid" class="text-red-500 text-sm font-normal">
-                    {{ error.$message }}
-                  </span>
+                  <ErrorMessage class="text-red-500 text-sm font-normal" name="total_supply" />
                 </div>
               </div>
 
-              <div class="w-full px-2 hidden py-2 rounded-md text-t16 text-white bg-[#ED1C24]">
-                Please note that the wallet must have established a trustline
-                for the specific token you are attempting to send.
+              <div>
+                <div class="flex items-center justify-between">
+                  <label for="issuer_wallet_private_key" class="block text-t16 font-normal leading-6 text-gray-900">Issuer Wallet Private Key
+                    <span class="text-red-500">*</span>
+                  </label>
+                </div>
+                <div class="mt-2">
+                  <Field 
+                  id="issuer_wallet_private_key" 
+                  name="issuer_wallet_private_key" 
+                  type="text"
+                  class="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                  <ErrorMessage class="text-red-500 text-sm font-normal" name="issuer_wallet_private_key" />
+                </div>
               </div>
 
               <div>
-                <button @click="submitForm" type="submit"
+                <div class="flex items-center justify-between">
+                  <label for="distributor_wallet_private_key" class="block text-t16 font-normal leading-6 text-gray-900">Distributor Wallet Private Key
+                    <span class="text-red-500">*</span>
+                  </label>
+                </div>
+                <div class="mt-2">
+                  <Field 
+                  id="distributor_wallet_private_key" 
+                  name="distributor_wallet_private_key" 
+                  type="text"
+                  class="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                  <ErrorMessage class="text-red-500 text-sm font-normal" name="distributor_wallet_private_key" />
+                </div>
+              </div>
+
+              <div>
+                <button  type="submit"
                   class="inline-flex bg-gradient justify-center rounded-full btn-padding text-sm font-semibold leading-6 text-white">
                   Generate Token
                 </button>
               </div>
-            </form>
+            </Form>
           </div>
         </div>
       </div>
@@ -74,79 +97,74 @@
 </template>
 
 <script setup>
-// import { useField } from 'vee-validate';
-// import { useForm } from 'vee-validate';
-// import * as Yup from "yup";
 import Layout from "@/components/Layout.vue";
 import Modal from '@/components/Modal.vue';
 import Toggle from '@/components/Toggle.vue';
-import { ref, defineProps, computed, reactive } from "vue";
-import axios from 'axios';
-import useVuelidate from '@vuelidate/core';
-import { required, maxLength, helpers } from '@vuelidate/validators';
-//importing function 
-import { Input} from "postcss";
 
+//importing function 
+import { ref } from "vue";
 //importing class of sweetalert2 library
+import axios from 'axios';
 import Swal from 'sweetalert2';
+import { Form , Field} from 'vee-validate';
+import * as Yup from "yup";
 
 
 const open = ref(false);
 
-const formData = reactive({
-  ticker: "",
-  total_supply: "",
+const schema = Yup.object({
+  ticker: Yup.string().required().max(4).label('Ticker'),
+  total_supply: Yup.string().required().label('Total Supply'),
+  issuer_wallet_private_key: Yup.string().required().min(56).label('Issuer Wallet Private Key'),
+  distributor_wallet_private_key: Yup.string().required().min(56).label('Distributor Wallet Private Key'),
 });
 
+const submitForm = (values) =>{
 
-const rules = computed(() =>{
-  return {
-    ticker: { 
-        required, 
-        maxLength:maxLength(4), 
-    },
-    total_supply: { 
-        required,
-    },
-  };
-});
+try {
+  // Show loading indicator
+  Swal.fire({
+    showConfirmButton: false,
+    title: 'Generating Token',
+    allowOutsideClick: false,
+    didOpen: () => {
+    Swal.showLoading()
+  },
+  });
 
-
-const v$ = useVuelidate (rules, formData); 
-
-const submitForm = async (e) =>{
-  e.preventDefault();
-  const result = await v$.value.$validate();
-  if(result){
-    try {
-
-      // Make the API call to generate the token
-      const response = await axios.post('api/generate_token', {
-        ticker: ticker.value,
-        total_supply: total_supply.value,
-      }, {
-        headers: {
-          'X-CSRF-TOKEN': window.Laravel.csrfToken,
-        }
-      });
-
-      if (response.data.status === 'success') {
-        Swal.fire({
-          icon: 'success',
-          title: 'Success!',
-          text: response.data.msg,
-        });
-      } else {
-        Swal.fire({
-          icon: 'error',
-          title: 'Error!',
-          text: response.data.msg,
-        });
-      }
-    } catch (error) {
-
+  axios.post('api/generate_token', values, {
+    headers: {
+      'X-CSRF-TOKEN': window.Laravel.csrfToken,
     }
-  }
+  }).then((response) => {
+    // Hide loading indicator
+    Swal.close();
+
+    if (response.data.status === 'success') {
+      Swal.fire({
+        icon: 'success',
+        title: 'Success!',
+        text: response.data.msg,
+      }).then(() => {
+        // Reset form values
+        const formData = reactive({
+          ticker: "",
+          total_supply: "",
+          issuer_wallet_private_key: "",
+          distributor_wallet_private_key: "",
+        });
+      });
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error!',
+        text: response.data.msg,
+      });
+    }
+  });
+} catch (error) {
+
+}
 };
 
 </script>
