1f44b650eecfb5b2a504ad18879be3cd96e87929
zainh332 <zain_h332@hotmail.com>
2023-08-04 16:15:22 +0500

    vite.config update

--- vite.config.js
+++ vite.config.js
@@ -24,15 +24,3 @@ export default defineConfig({
       }
     }
 })
-
-
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//     plugins: [vue()],
-//     resolve: {
-//       alias: {
-//         '@': path.resolve(__dirname, 'src')
-//       }
-//     }
-//   })
