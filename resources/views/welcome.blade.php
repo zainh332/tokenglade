08a5ceebc0b167e47f34f4cbfae807730e879105
zainh332 <zain_h332@hotmail.com>
2023-08-04 16:04:40 +0500

    welcome page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        {{-- <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>TokenGlade</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-        {{-- @vite(['../resources/js/app.js', '../resources/css/app.css']) --}}
         @vite(['../src/main.js', '../resources/css/app.css'])
         <script>
             window.Laravel = {!! json_encode([
