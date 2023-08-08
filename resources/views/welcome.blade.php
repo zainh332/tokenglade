5147545380cef95b89f6ffbf498b22f20d504a13
zainh332 <zain_h332@hotmail.com>
2023-08-09 01:14:04 +0500

    Minor Changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @vite(['src/main.js', 'resources/css/app.css'])
-        
         <link rel="shortcut icon" type="image/x-icon" href="{{ asset('build/assets/flower-9bebdb58.png') }}">
     </head>
     <body class="antialiased">
