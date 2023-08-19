c08f905f670fda3d8922184e064f4f96fa4006c4
zainh332 <zain_h332@hotmail.com>
2023-08-19 18:57:52 +0500

    google saearch console setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @vite(['src/main.js', 'resources/css/app.css'])
         <link rel="shortcut icon" type="image/x-icon" href="{{ asset('build/assets/flower-9bebdb58.png') }}">
+        <meta name="google-site-verification" content="efg2XUb3x3NC7DsUUkEtjtQ2k5ewYAMGt-rkqAjlQDM" />
     </head>
     <body class="antialiased">
        <div id="app"></div>
