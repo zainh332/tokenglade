5b05cc972e1c752d62a82e3fb399aeca73e16154
zainh332 <zain_h332@hotmail.com>
2025-03-01 14:40:20 +0500

    Added global helper function to check XLM balance

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,33 @@
+<?php
+use Soneso\StellarSDK\StellarSDK;
+use Soneso\StellarSDK\Exceptions\HorizonRequestException;
+use Exception;
+
+
+/**
+ * Check the XLM balance of a Stellar wallet
+ */
+function checkXlmBalance(string $publicKey): ?float
+{
+    try {
+        $sdk = StellarSDK::getPublicNetInstance();
+        $account = $sdk->requestAccount($publicKey);
+
+        foreach ($account->getBalances() as $balance) {
+            if ($balance->getAssetType() === 'native') {
+                return (float) $balance->getBalance();
+            }
+        }
+
+        return 0.0; // If no native balance found, return 0
+
+    } catch (HorizonRequestException $e) {
+        if ($e->getStatusCode() == 404) {
+            return false;
+        }
+        throw $e;
+    }
+    catch (\Exception $e) {
+        return null; 
+    }
+}
