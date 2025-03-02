dd6faa7f157fb9173748625b796d2b48bd70e26b
zainh332 <zain_h332@hotmail.com>
2025-03-03 02:59:20 +0500

    fix

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            $table->string('tokenable_type', 191); // Define length explicitly
+            $table->unsignedBigInteger('tokenable_id');
+            $table->index(['tokenable_type', 'tokenable_id']); // Avoids key length issue
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
