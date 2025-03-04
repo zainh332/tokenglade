f820f26c04c1a0be9227335f1ee772a4e955a639
zainh332 <zain_h332@hotmail.com>
2025-03-05 02:41:54 +0500

    footer fixed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -114,7 +114,7 @@
     </div>
 
     <div class="flex flex-col items-center justify-center gap-6 mt-24 mb-8 md:flex-row md:justify-between">
-      <p class="font-normal text-t16">© Copyright by 2024 - TokenGlade</p>
+      <p class="font-normal text-t16">© Copyright by 2025 - TokenGlade</p>
     
       <div class="flex flex-col items-center gap-4 sm:flex-row">
         <router-link class="font-normal text-t16" to="/privacy-policy">Privacy Policy</router-link>
