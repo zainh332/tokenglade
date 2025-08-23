c6243e2b7424e83da1f2f950359f4b47cc95377f
zainh332 <zain_h332@hotmail.com>
2025-08-24 01:42:29 +0500

    testing

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,14 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+const token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
