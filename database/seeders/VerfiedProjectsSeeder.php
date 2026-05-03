d202e5f5067ff9b8b49aaf748a41fd86278d5980
zainh332 <zain_h332@hotmail.com>
2026-05-03 20:48:05 +0500

    aqua verified

--- database/seeders/VerfiedProjectsSeeder.php
+++ database/seeders/VerfiedProjectsSeeder.php
@@ -13,12 +13,22 @@ class VerfiedProjectsSeeder extends Seeder
      */
     public function run(): void
     {
+        VerifiedProject::truncate();
+
         $projects = [
             [
                 'blockchain_id' => 1,
                 'identifier' => 'GAM3PID2IOBTNCBMJXHIAS4EO3GQXAGRX4UB6HTQY2DUOVL3AQRB4UKQ',
                 'asset_code' => 'TKG',
                 'name' => 'TokenGlade',
+                'status' => 1,
+            ],
+
+            [
+                'blockchain_id' => 1,
+                'identifier' => 'GBNZILSTVQZ4R7IKQDGHYGY2QXL5QOFJYQMXPKWRRM5PAV7Y4M67AQUA',
+                'asset_code' => 'AQUA',
+                'name' => 'Aquarius',
                 'status' => 1,
             ],
         ];
