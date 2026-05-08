3eb4c980e132a7af7eb79db701614636a40110c9
zainh332 <zain_h332@hotmail.com>
2026-05-08 23:53:22 +0500

    lumenswap verified

--- database/seeders/VerfiedProjectsSeeder.php
+++ database/seeders/VerfiedProjectsSeeder.php
@@ -39,6 +39,14 @@ class VerfiedProjectsSeeder extends Seeder
                 'name' => 'Stronghold',
                 'status' => 1,
             ],
+
+            [
+                'blockchain_id' => 1,
+                'identifier' => 'GAB7STHVD5BDH3EEYXPI3OM7PCS4V443PYB5FNT6CFGJVPDLMKDM24WK',
+                'asset_code' => 'LSP',
+                'name' => 'Lumenswap',
+                'status' => 1,
+            ],
         ];
 
         foreach ($projects as $project) {
