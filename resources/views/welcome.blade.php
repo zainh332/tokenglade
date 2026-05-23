e97915f788586fbf04d1d55c47f445e3d1e9bda2
zainh332 <zain_h332@hotmail.com>
2026-05-24 03:22:50 +0500

    Fix logo path

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @vite(['src/main.js', 'resources/css/app.css'])
-        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('build/assets/flower-9bebdb58.png') }}">
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('build/assets/token-glade-logo.png') }}">
         <meta name="google-site-verification" content="efg2XUb3x3NC7DsUUkEtjtQ2k5ewYAMGt-rkqAjlQDM" />
         <script src="https://unpkg.com/@albedo-link/intent"></script>
     </head>
