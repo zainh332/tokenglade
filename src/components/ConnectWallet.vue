838033f1bcc1cdc4848a1054c0093d5321f158d1
zainh332 <zain_h332@hotmail.com>
2026-06-26 06:03:41 +0500

    Connect Wallet modal on mobile screen alignment

--- src/components/ConnectWallet.vue
+++ src/components/ConnectWallet.vue
@@ -7,7 +7,7 @@
             </TransitionChild>
 
             <div class="fixed inset-0 z-50 w-screen overflow-y-auto">
-                <div class="flex items-end justify-center min-h-full p-2 text-center sm:items-center sm:p-0">
+                <div class="flex items-center justify-center min-h-full p-4 text-center">
                     <TransitionChild as="template" enter="ease-out duration-300"
                         enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
