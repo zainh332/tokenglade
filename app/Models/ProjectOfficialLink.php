eb2905994c6e0c6f22378393fe20b8924e47b4ae
zainh332 <zain_h332@hotmail.com>
2026-08-03 02:42:25 +0500

    by default https will be added in tables

--- app/Models/ProjectOfficialLink.php
+++ app/Models/ProjectOfficialLink.php
@@ -15,4 +15,39 @@ class ProjectOfficialLink extends Model
     {
         return $this->belongsTo(ProjectProfile::class, 'project_profile_id');
     }
+
+    protected function formatUrlWithProtocol($url)
+    {
+        if (!$url) return null;
+        $url = trim($url);
+        if ($url !== '' && !preg_match('/^https?:\/\//i', $url)) {
+            return 'https://' . $url;
+        }
+        return $url;
+    }
+
+    public function setWebsiteAttribute($value)
+    {
+        $this->attributes['website'] = $this->formatUrlWithProtocol($value);
+    }
+
+    public function setDocumentationAttribute($value)
+    {
+        $this->attributes['documentation'] = $this->formatUrlWithProtocol($value);
+    }
+
+    public function setWhitepaperAttribute($value)
+    {
+        $this->attributes['whitepaper'] = $this->formatUrlWithProtocol($value);
+    }
+
+    public function setGithubAttribute($value)
+    {
+        $this->attributes['github'] = $this->formatUrlWithProtocol($value);
+    }
+
+    public function setMediumAttribute($value)
+    {
+        $this->attributes['medium'] = $this->formatUrlWithProtocol($value);
+    }
 }
--- app/Models/ProjectSocialLink.php
+++ app/Models/ProjectSocialLink.php
@@ -15,4 +15,44 @@ class ProjectSocialLink extends Model
     {
         return $this->belongsTo(ProjectProfile::class, 'project_profile_id');
     }
+
+    protected function formatUrlWithProtocol($url)
+    {
+        if (!$url) return null;
+        $url = trim($url);
+        if ($url !== '' && !preg_match('/^https?:\/\//i', $url)) {
+            return 'https://' . $url;
+        }
+        return $url;
+    }
+
+    public function setTwitterAttribute($value)
+    {
+        $this->attributes['twitter'] = $this->formatUrlWithProtocol($value);
+    }
+
+    public function setTelegramAttribute($value)
+    {
+        $this->attributes['telegram'] = $this->formatUrlWithProtocol($value);
+    }
+
+    public function setDiscordAttribute($value)
+    {
+        $this->attributes['discord'] = $this->formatUrlWithProtocol($value);
+    }
+
+    public function setLinkedinAttribute($value)
+    {
+        $this->attributes['linkedin'] = $this->formatUrlWithProtocol($value);
+    }
+
+    public function setRedditAttribute($value)
+    {
+        $this->attributes['reddit'] = $this->formatUrlWithProtocol($value);
+    }
+
+    public function setYoutubeAttribute($value)
+    {
+        $this->attributes['youtube'] = $this->formatUrlWithProtocol($value);
+    }
 }
